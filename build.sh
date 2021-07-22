f264a3649dd5da3c79bb38e89fbda2119d9a4bf8
Divyanshu-Modi <divyan.m05@gmail.com>
2021-07-22 16:39:23 +0530

    Builder: Update to version 2.0
     Changes:
      -> Cleaned up telegram posts
      -> Removed useless spinner shown while cloning
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- build.sh
+++ build.sh
@@ -1,76 +1,96 @@
 #bin/#!/bin/bash
 
-    KERNEL_DIR=$HOME/Kernel
-    cd $KERNEL_DIR
-    USER=OGIndian
-    HOST=Nucleus
-    GCC_PATH=$HOME/gcc-arm64
-    GCC_ARM32=$HOME/gcc-arm32/bin/arm-eabi-
-    DEVICE=tulip
-    BUILD=clean
-    CAM_LIB=
-    ZIP_DIR=$HOME/Repack
-    AKSH=$ZIP_DIR/anykernel.sh
-    DFCF=AtomX-$DEVICE${CAM_LIB}_defconfig
-    CONFIG=$KERNEL_DIR/arch/arm64/configs/$DFCF
-    mkdir out
-    OUT=$KERNEL_DIR/out
-    mkdir work
+# Device config
+	DEVICE=tulip
+	BUILD=clean
+	NAME='Redmi Note 6 Pro'
+	CAM_LIB=
 
-    telegram-send "Building Kernel for $DEVICE"
-if [[ "$CAM_LIB" != "" ]]; then
-    telegram-send "Camlib: $CAM_LIB"
-fi
-    BUILD_START=$(date +"%s")
+# User config
+	USER=OGIndian
+	HOST=$(uname -n)
+	VERSION=2.0
+
+# Path
+	KERNEL_DIR=$HOME/Kernel
+	GCC_PATH=$HOME/gcc-arm64
+	GCC_ARM32=$HOME/gcc-arm32/bin/arm-eabi-
+	ZIP_DIR=$HOME/Repack
+	AKSH=$ZIP_DIR/anykernel.sh
+	OUT=$KERNEL_DIR/out
+	cd $KERNEL_DIR
+
+# Defconfig
+	DFCF=AtomX-$DEVICE${CAM_LIB}_defconfig
+	CONFIG=$KERNEL_DIR/arch/arm64/configs/$DFCF
 
 muke() {
-    make O=work $CFLAG ARCH=arm64 -s    \
-      LLVM=1                            \
-      HOSTCC=gcc                        \
-      HOSTLD=ld.lld                     \
-      CC=aarch64-elf-gcc                \
-      HOSTCXX=aarch64-elf-g++           \
-      KBUILD_BUILD_USER=$USER           \
-      KBUILD_BUILD_HOST=$HOST           \
-      PATH=$GCC_PATH/bin:$PATH          \
-      CROSS_COMPILE_COMPAT=$GCC_ARM32   \
-      LD_LIBRARY_PATH=$GCC_PATH/lib:$LD_LIBRARY_PATH
+	make O=work $CFLAG ARCH=arm64         \
+		LLVM=1                            \
+		HOSTCC=gcc                        \
+		HOSTLD=ld.lld                     \
+		CC=aarch64-elf-gcc                \
+		HOSTCXX=aarch64-elf-g++           \
+		KBUILD_BUILD_USER=$USER           \
+		KBUILD_BUILD_HOST=$HOST           \
+		PATH=$GCC_PATH/bin:$PATH          \
+		CROSS_COMPILE_COMPAT=$GCC_ARM32   \
+		LD_LIBRARY_PATH=$GCC_PATH/lib:$LD_LIBRARY_PATH
 }
 
-    sed -i '/CONFIG_JUMP_LABEL/ a CONFIG_LTO_GCC=y' $CONFIG
-    sed -i '/CONFIG_JUMP_LABEL/ a CONFIG_OPTIMIZE_INLINING=y' $CONFIG
-    CFLAG=$DFCF
-    muke
+	BUILD_START=$(date +"%s")
 
-    CFLAG=-j$(nproc)
-    muke
+	sed -i '/CONFIG_JUMP_LABEL/ a CONFIG_LTO_GCC=y' $CONFIG
+	sed -i '/CONFIG_JUMP_LABEL/ a CONFIG_OPTIMIZE_INLINING=y' $CONFIG
+	CFLAG=$DFCF
+	muke
+
+	CFLAG=-j$(nproc)
+	muke
 
 if [[ -f $KERNEL_DIR/work/arch/arm64/boot/Image.gz-dtb ]]; then
-    FINAL_ZIP="$DEVICE$CAM_LIB-AtomX-EAS-GCC_LTO-`date +"%Y%m%d-%H%M"`"
-    cd $ZIP_DIR
-    cp $KERNEL_DIR/work/arch/arm64/boot/Image.gz-dtb $ZIP_DIR/
-    sed -i s/demo1/$DEVICE/g $AKSH
-    if [[ "$DEVICE2" ]]; then
-        sed -i /device.name1/ a device.name2=$DEVICE2 $AKSH
-    fi
-    zip -qr9 "$FINAL_ZIP".zip * -x README.md *placeholder zipsigner*
-    java -jar zipsigner* "$FINAL_ZIP".zip "$FINAL_ZIP"-signed.zip
-    cp "$FINAL_ZIP"-signed.zip $OUT
-    cd $KERNEL_DIR
-    rm $ZIP_DIR/*.zip $ZIP_DIR/Image.gz-dtb
-    BUILD_END=$(date +"%s")
-    DIFF=$(($BUILD_END - $BUILD_START))
-    telegram-send "Compiled for $DEVICE$CAM_LIB in $(($DIFF / 60)).$(($DIFF % 60)) mins."
-    final=\
-"Build Date: $(date)
-""
-"*******Changelog*******"
-""
-$(git log HEAD~10..HEAD --pretty='format:%C(auto) -> %s')"
-    telegram-send --file $OUT/*.zip
-    telegram-send "$final"
-    exit
+	FINAL_ZIP="$DEVICE$CAM_LIB-AtomX-EAS-GCC_LTO-`date +"%H%M"`"
+	cd $ZIP_DIR
+	cp $KERNEL_DIR/work/arch/arm64/boot/Image.gz-dtb $ZIP_DIR/
+	sed -i s/demo1/$DEVICE/g $AKSH
+	if [[ "$DEVICE2" ]]; then
+		sed -i /device.name1/ a device.name2=$DEVICE2 $AKSH
+	fi
+	zip -qr9 ${FINAL_ZIP}.zip * -x README.md *placeholder zipsigner*
+	java -jar zipsigner* ${FINAL_ZIP}.zip ${FINAL_ZIP}-signed.zip
+	mkdir out
+	cp ${FINAL_ZIP}-signed.zip $OUT
+	cd $KERNEL_DIR
+	rm $ZIP_DIR/*.zip $ZIP_DIR/Image.gz-dtb
+
+	BUILD_END=$(date +"%s")
+
+	DIFF=$(($BUILD_END - $BUILD_START))
+
+	if [[ "$CAM_LIB" == "" ]]; then
+		CAM=OLD-CAM
+	else
+		CAM=$CAM_LIB
+	fi
+
+	source work/.config
+	telegram-send --format html "\
+	***************Atom-X-Kernel***************
+	Compiler: <code>$CONFIG_CC_VERSION_TEXT</code>
+	Linux Version: <code>$(make kernelversion)</code>
+	Builder Version: <code>$VERSION</code>
+	Maintainer: <code>$USER</code>
+	Device: <code>$NAME</code>
+	Codename: <code>$DEVICE</code>
+	Camlib: <code>$CAM</code>
+	Build Date: <code>$(date +"%Y-%m-%d %H:%M")</code>
+	Build Duration: <code>$(($DIFF / 60)).$(($DIFF % 60)) mins</code>
+	Changelog: <a href='$SOURCE'> Here </a>"
+	telegram-send -file $OUT/"${FINAL_ZIP}-signed.zip"
+
+	exit
 else
-    telegram-send "Error! Compilaton failed: Kernel Image missing"
-    exit 1
+	telegram-send "Error⚠️ Compilaton failed: Kernel Image missing"
+
+	exit 1
 fi
