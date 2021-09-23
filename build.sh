bab05dc4f443f57d26142dcc4a903893fb5edd1a
Divyanshu-Modi <divyan.m05@gmail.com>
2021-09-23 13:52:41 +0530

    Builder: script clean up
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- build.sh
+++ build.sh
@@ -1,42 +1,48 @@
-# SPDX-License-Identifier: GPL-3.0
-# Copyright (c) 2021, Divyanshu-Modi <divyan.m05@gmail.com>
 #bin/#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0
+# Made by Divyanshu-Modi
+# Revision: 18-09-2021 V1
 
-	COMPILER="$1"
+	VERSION='8.0'
+	COMPILER='clang'
+
+# USER
 	USER='OGIndian'
-	HOST="$(uname -n)"
-	VERSION='6.0'
+	HOST='AtomX-Drone'
+
+# DEVICE CONFIG
 	DEVICENAME='Redmi Note 6 Pro'
-	DEVICE='tulip'
+	DEVICE="$1"
+	DEVICE2="$2"
+
+# PATH
 	KERNEL_DIR="$HOME/Kernel"
 	ZIP_DIR="$HOME/Repack"
 	AKSH="$ZIP_DIR/anykernel.sh"
-	DFCF="AtomX-${DEVICE}_defconfig"
-	if [[ ! -f $KERNEL_DIR/arch/arm64/configs/$DFCF ]]; then
-		DFCF="${DEVICE}-perf_defconfig"
-		if [[ ! -f $KERNEL_DIR/arch/arm64/configs/$DFCF ]]; then
-			DFCF="${DEVICE}_defconfig"
-        fi
-	fi
-	CONFIG="$KERNEL_DIR/arch/arm64/configs/$DFCF"
-	mkdir $COMPILER
+
+# DEFCONFIG
+	DFCF="${DEVICE}-perf_defconfig"
+	CONFIG="$KERNEL_DIR/arch/arm64/configs/vendor/$DFCF"
 
 # Set variables
-	if [[ "$COMPILER" == "CLANG" ]]; then
-		CC='clang'
-		HOSTCC='clang'
-		HOSTCXX='clang++'
-		C_PATH="$HOME/clang"
-		CC_64='aarch64-linux-gnu-'
-		CC_COMPAT='arm-linux-gnueabi-'
-	elif [[ "$COMPILER" == "GCC" ]]; then
-		HOSTCC='gcc'
-		CC_64='aarch64-elf-'
-		CC='aarch64-elf-gcc'
-		C_PATH="$HOME/gcc-arm64"
-		HOSTCXX='aarch64-elf-g++'
-		CC_COMPAT="$HOME/gcc-arm32/bin/arm-eabi-"
-	fi
+	case $COMPILER in
+		clang)
+			CC='clang'
+			HOSTCC="$CC"
+			HOSTCXX="$CC++"
+			CC_64='aarch64-linux-gnu-'
+			CC_COMPAT='arm-linux-gnueabi-'
+			C_PATH="$HOME/clang"
+		;;
+		gcc)
+			HOSTCC='gcc'
+			CC_64='aarch64-elf-'
+			CC='aarch64-elf-gcc'
+			CC_COMPAT='arm-eabi-'
+			HOSTCXX='aarch64-elf-g++'
+			C_PATH="$HOME/gcc-arm64/bin:$HOME/gcc-arm32/"
+		;;
+	esac
 
 	muke() {
 		make O=$COMPILER $CFLAG ARCH=arm64 \
@@ -44,73 +50,88 @@
 			CC=$CC                         \
 			LLVM=1                         \
 			LLVM_IAS=1                     \
-			CROSS_COMPILE=$CC_64           \
 			HOSTCC=$HOSTCC                 \
 			HOSTCXX=$HOSTCXX               \
+			CROSS_COMPILE=$CC_64           \
 			PATH=$C_PATH/bin:$PATH         \
 			KBUILD_BUILD_USER=$USER        \
 			KBUILD_BUILD_HOST=$HOST        \
-			CROSS_COMPILE_ARM32=$CC_COMPAT \
 			CROSS_COMPILE_COMPAT=$CC_COMPAT\
 			LD_LIBRARY_PATH=$C_PATH/lib:$LD_LIBRARY_PATH
 	}
 
-	BUILD_START=$(date +"%s")
+	sed -i '/CONFIG_LLVM_POLLY=y/ a CONFIG_LTO_CLANG_FULL=y' $CONFIG
 
 	CFLAG=$DFCF
 	muke
+
 	source $COMPILER/.config
-	if [[ "CONFIG_LTO_CLANG_FULL=y" ]]; then
-		VARIANT=FULL_LTO
-	elif [[ "CONFIG_LTO_CLANG_THIN=y" ]]; then
-		VARIANT=THIN_LTO
+	if [[ "$CONFIG_LTO_CLANG_FULL" == "y" ]]; then
+		VARIANT='FULL_LTO'
+	elif [[ "$CONFIG_LTO_CLANG_THIN" == "y" ]]; then
+		VARIANT='THIN_LTO'
 	else
-		VARIANT=NON_LTO
+		VARIANT='NON_LTO'
 	fi
 	telegram-send --format html "Building: <code>$VARIANT</code>"
+
+	BUILD_START=$(date +"%s")
 
 	CFLAG=-j$(nproc)
 	muke
 
+	BUILD_END=$(date +"%s")
+
 	if [[ -f $KERNEL_DIR/$COMPILER/arch/arm64/boot/Image.gz-dtb ]]; then
 		FDEVICE=${DEVICE^^}
 		KNAME=$(echo "$CONFIG_LOCALVERSION" | cut -c 2-)
-		FINAL_ZIP="$KNAME-$FDEVICE-`date +"%H%M"`"
-		cd $ZIP_DIR
+		KV=$(cat $KERNEL_DIR/work/include/generated/utsrelease.h | cut -c 21- | tr -d '"')
+
 		cp $KERNEL_DIR/$COMPILER/arch/arm64/boot/Image.gz-dtb $ZIP_DIR/
+
 		sed -i "s/demo1/$DEVICE/g" $AKSH
 		if [[ "$DEVICE2" ]]; then
 			sed -i "/device.name1/ a device.name2=$DEVICE2" $AKSH
 		fi
+
+		cd $ZIP_DIR
+
+		FINAL_ZIP="$KNAME-$FDEVICE-$VARIANT-`date +"%H%M"`"
+
 		zip -r9 "$FINAL_ZIP".zip * -x README.md *placeholder zipsigner*
+
 		java -jar zipsigner* "$FINAL_ZIP.zip" "$FINAL_ZIP-signed.zip"
+
 		FINAL_ZIP="$FINAL_ZIP-signed.zip"
+
 		telegram-send --file $ZIP_DIR/$FINAL_ZIP
+
 		rm *.zip Image.gz-dtb
+
+		cd $KERNEL_DIR
+
 		sed -i "s/$DEVICE/demo1/g" $AKSH
 		if [[ "$DEVICE2" ]]; then
 			sed -i "/device.name2/d" $AKSH
 		fi
 
-		BUILD_END=$(date +"%s")
 		DIFF=$(($BUILD_END - $BUILD_START))
-
-		cd $KERNEL_DIR
 		COMPILER_NAME="$($C_PATH/bin/$CC --version 2>/dev/null | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+		HEAD="$(git log --oneline HEAD~2..HEAD~ --decorate=auto)"
 
 		telegram-send --disable-web-page-preview --format html "\
 		**************Atom-X-Kernel**************
 		Compiler: <code>$COMPILER</code>
 		Compiler-name: <code>$COMPILER_NAME</code>
-		Linux Version: <code>$(make kernelversion)</code>
+		Linux Version: <code>$KV</code>
 		Builder Version: <code>$VERSION</code>
 		Build Type: <code>$VARIANT</code>
 		Maintainer: <code>$USER</code>
 		Device: <code>$DEVICENAME</code>
 		Codename: <code>$DEVICE</code>
-		Camlib: <code>$CAM</code>
 		Build Date: <code>$(date +"%Y-%m-%d %H:%M")</code>
 		Build Duration: <code>$(($DIFF / 60)).$(($DIFF % 60)) mins</code>
+		Commit Head: <code>$HEAD</code>
 		Changelog: <a href='$SOURCE'> Here </a>"
 	else
 		telegram-send "Error⚠️ $COMPILER failed to build"
--- builder.sh
+++ builder.sh
@@ -1,10 +1,12 @@
 #bin/#!/bin/bash
 
-	git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm64 -b  gcc-master $HOME/gcc-arm64
-	git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
-	git clone -q --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang $HOME/clang
-	git clone -q --depth=1 https://github.com/Divyanshu-Modi/AnyKernel3 $HOME/Repack
-	git clone -q --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_sdm660 $HOME/Kernel
+	dir_get() {
+		git clone --progress -q $1 $2
+	}
+
+	dir_get https://gitlab.com/ElectroPerf/atom-x-clang $HOME/clang
+	dir_get https://github.com/Divyanshu-Modi/AnyKernel3 $HOME/Repack
+	dir_get https://github.com/Atom-X-Devs/android_kernel_xiaomi_sdm660 $HOME/Kernel
 	pip3 -q install telegram-send
 
 	mkdir $HOME/.config
@@ -12,9 +14,8 @@
 	sed -i s/demo1/${BOT_API_KEY}/g $HOME/.config/telegram-send.conf
 	sed -i s/demo2/${CHAT_ID}/g $HOME/.config/telegram-send.conf
 	mv build.sh $HOME/Kernel/build.sh
+	cd $HOME/clang && git checkout 06c20aff3cd0b463a5603dc7dedd47d64a6a8b8b
 
-	cd $HOME/Kernel
-	bash build.sh CLANG
-#	bash build.sh GCC
+	cd $HOME/Kernel && bash build.sh tulip tulix
 
 	exit
