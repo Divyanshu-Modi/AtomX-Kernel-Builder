cdaf132fa7707c66769f0c04678a20e21f1cd499
@Divyanshu775 <67594857+Divyanshu-Modi@users.noreply.github.com>
2022-10-14 19:25:31 +0530

    Update AtomX.sh

--- AtomX.sh
+++ AtomX.sh
@@ -6,10 +6,6 @@
 KBUILD_USER="AtomX"
 KBUILD_HOST="Drone"
 
-# Build type (Fresh build: clean | incremental build: dirty)
-# (default: dirty | modes: clean, dirty)
-BUILD='clean'
-
 ############################################################################
 
 ########################    DIRECTOR PATHS   ###############################
@@ -49,6 +45,9 @@ inform() {
 }
 
 muke() {
+	if [[ -z $COMPILER || -z $COMPILER32 ]]; then
+		error "Compiler is missing"
+	fi
 	if ! make $@ ${MAKE_ARGS[@]} $FLAG; then
 		error "make failed"
 	fi
@@ -66,27 +65,34 @@ usage() {
 
 compiler_setup() {
 ############################  COMPILER SETUP  ##############################
-	MAKE_ARGS=()
 	# default to clang
 	CC='clang'
 	C_PATH="$TLDR/$CC"
 	LLVM_PATH="$C_PATH/bin"
 	if [[ $COMPILER == gcc ]]; then
 		# Just override the existing declarations
-		CC='aarch64-linux-gnu-gcc'
+		CC='aarch64-elf-gcc'
 		C_PATH="$TLDR/gcc-arm64"
 	fi
+
+	C_NAME=$("$LLVM_PATH"/$CC --version | head -n 1 | perl -pe 's/\(http.*?\)//gs')
+	if [[ $COMPILER32 == "gcc" ]]; then
+		MAKE_ARGS+=("CC_COMPAT=$TLDR/gcc-arm/bin/arm-eabi-gcc" "CROSS_COMPILE_COMPAT=$TLDR/gcc-arm/bin/arm-eabi-")
+		C_NAME_32=$($(echo "${MAKE_ARGS[@]}" | sed s/' '/'\n'/g | grep CC_COMPAT | cut -c 11-) --version | head -n 1)
+	else
+		MAKE_ARGS+=("CROSS_COMPILE_COMPAT=arm-linux-gnueabi-")
+		C_NAME_32="$C_NAME"
+	fi
+
 	MAKE_ARGS+=("O=work"
-				"ARCH=arm64"
-				"LLVM=$LLVM_PATH"
-				"HOSTLD=ld.lld" "CC=$CC"
-				"PATH=$C_PATH/bin:$PATH"
-				"KBUILD_BUILD_USER=$KBUILD_USER"
-				"KBUILD_BUILD_HOST=$KBUILD_HOST"
-				"CROSS_COMPILE=aarch64-linux-gnu-"
-				"CC_COMPAT=$TLDR/gcc-arm/bin/arm-eabi-gcc"
-				"LD_LIBRARY_PATH=$C_PATH/lib:$LD_LIBRARY_PATH"
-				"CROSS_COMPILE_COMPAT=$TLDR/gcc-arm/bin/arm-eabi-")
+		"ARCH=arm64"
+		"LLVM=$LLVM_PATH"
+		"HOSTLD=ld.lld" "CC=$CC"
+		"PATH=$C_PATH/bin:$PATH"
+		"KBUILD_BUILD_USER=$KBUILD_USER"
+		"KBUILD_BUILD_HOST=$KBUILD_HOST"
+		"CROSS_COMPILE=aarch64-linux-gnu-"
+		"LD_LIBRARY_PATH=$C_PATH/lib:$LD_LIBRARY_PATH")
 ############################################################################
 }
 
@@ -96,44 +102,31 @@ kernel_builder() {
 		error 'Codename not present connot proceed'
 		exit 1
 	fi
-
-	case $BUILD in
-		clean)
-			rm -rf work || mkdir work
-		;;
-		*)
-			muke clean mrproper distclean
-		;;
-	esac
+	if [[ $BUILD == "clean" ]]; then
+		inform "Cleaning work directory, please wait...."
+		muke -s clean mrproper distclean
+	fi
 
 	# Build Start
 	BUILD_START=$(date +"%s")
 
-	DFCF="vendor/${CODENAME}-${SUFFIX}_defconfig"
-
-	if [[ $PIXEL_THERMALS == "1" ]]; then
-		./scripts/config --file arch/arm64/configs/vendor/lisa-qgki_defconfig -d MI_THERMAL_CPU_THROTTLE
-		THERMAL="Pixel Thermal"
-	else
-		THERMAL="XiaoMi Thermal"
-	fi
-
 	# Make .config
-	muke $DFCF
+	muke "vendor/${CODENAME}-${SUFFIX}_defconfig"
 
 	source work/.config
-	C_NAME=$(echo $CONFIG_CC_VERSION_TEXT | head -n 1 | perl -pe 's/\(http.*?\)//gs')
-	C_NAME_32=$($(echo ${MAKE_ARGS[@]} | sed s/' '/'\n'/g | grep CC_COMPAT | cut -c 11-) --version | head -n 1)
+	MOD_NAME="$(muke kernelrelease -s)"
+	KERNEL_VERSION=$(echo "$MOD_NAME" | cut -c -7)
 
 	inform "
 		*************Build Triggered*************
 		Date: <code>$(date +"%Y-%m-%d %H:%M")</code>
 		Build Number: <code>$DRONE_BUILD_NUMBER</code>
+		Linux Version: <code>$KERNEL_VERSION</code>
+		Kernel Name: <code>$MOD_NAME</code>
 		Device: <code>$DEVICENAME</code>
 		Codename: <code>$CODENAME</code>
 		Compiler: <code>$C_NAME</code>
 		Compiler_32: <code>$C_NAME_32</code>
-		Thermal Support: <code>$THERMAL</code>
 	"
 
 	# Compile
@@ -151,50 +144,56 @@ kernel_builder() {
 
 	DIFF=$(($BUILD_END - $BUILD_START))
 
-	if [[ -f $KERNEL_DIR/work/arch/arm64/boot/$TARGET ]]; then
-		zipper
-	else
-		error 'Kernel image not found'
-	fi
+	zipper
 ############################################################################
 }
 
 zipper() {
 ####################################  ZIP  #################################
+	TARGET="$(muke image_name -s)"
+	if [[ ! -f $KERNEL_DIR/work/$TARGET ]]; then
+		error 'Kernel image not found'
+	fi
 	if [[ ! -d $AK3_DIR ]]; then
 		error 'Anykernel not present cannot zip'
 	fi
 	if [[ ! -d "$KERNEL_DIR/out" ]]; then
-		mkdir $KERNEL_DIR/out
+		mkdir "$KERNEL_DIR"/out
 	fi
 
-	cp $KERNEL_DIR/work/arch/arm64/boot/$TARGET $AK3_DIR
-	cp $DTB_PATH/*.dtb $AK3_DIR/dtb
-	cp $DTB_PATH/*.img $AK3_DIR/
+	# Making sure everything is ok before making zip
+	cd "$AK3_DIR" || exit
+	make clean
+	cd "$KERNEL_DIR" || exit
+
+	cp "$KERNEL_DIR"/work/"$TARGET" "$AK3_DIR"
+	cp "$DTB_PATH"/*.dtb "$AK3_DIR"/dtb
+	cp "$DTB_PATH"/*.img "$AK3_DIR"/
+
 	if [[ $CONFIG_MODULES == "y" ]]; then
-		MOD_NAME="$(cat work/include/generated/utsrelease.h | cut -c 21- | tr -d '"')"
 		MOD_PATH="work/modules/lib/modules/$MOD_NAME"
 
-		cp $(find $MOD_PATH -name '*.ko') $AKVDR
-		cp $MOD_PATH/modules.{alias,dep,softdep} $AKVDR
-		cp $MOD_PATH/modules.order $AKVDR/modules.load
-		sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' $AKVDR/modules.dep
-		sed -i 's/.*\///g' $AKVDR/modules.load
+		sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' "$MOD_PATH"/modules.dep
+		sed -i 's/.*\///g' "$MOD_PATH"/modules.order
+
+		# shellcheck disable=SC2046
+		# cp breaks with advised follow up
+		cp $(find "$MOD_PATH" -name '*.ko') "$AKVDR"/
+		cp "$MOD_PATH"/modules.{alias,dep,softdep} "$AKVDR"/
+		cp "$MOD_PATH"/modules.order "$AKVDR"/modules.load
 	fi
 
-	VERSION=`echo $CONFIG_LOCALVERSION | cut -c 8-`
-	KERNEL_VERSION=$(make kernelversion)
 	LAST_COMMIT=$(git show -s --format=%s)
 	LAST_HASH=$(git rev-parse --short HEAD)
 
-	cd $AK3_DIR
+	cd "$AK3_DIR" || exit
 
-	make zip VERSION="$VERSION"
+	make zip VERSION="$(echo "$CONFIG_LOCALVERSION" | cut -c 8-)" CUSTOM="$LAST_HASH"
 
 	inform "
 		*************AtomX-Kernel*************
 		Linux Version: <code>$KERNEL_VERSION</code>
-		AtomX-Version: <code>$VERSION</code>
+		Kernel Name: <code>$MOD_NAME</code>
 		CI: <code>$KBUILD_HOST</code>
 		Core count: <code>$(nproc)</code>
 		Compiler: <code>$C_NAME</code>
@@ -224,21 +223,27 @@ zipper() {
 if [[ -z $* ]]; then
 	usage
 fi
-
+if [[ $* =~ "--silence" ]]; then
+	MAKE_ARGS+=("-s")
+fi
 for arg in "$@"; do
 	case "${arg}" in
 		"--compiler="*)
 			COMPILER=${arg#*=}
 			COMPILER=${COMPILER,,}
-			case $COMPILER in
-				clang | gcc)
-					compiler_setup
-				;;
-				*)
-					usage
-				;;
-			esac
-		;;
+			if [[ -z $COMPILER ]]; then
+				usage
+				break
+			fi
+			;&
+		"--compiler32="*)
+			COMPILER32=${arg#*=}
+			COMPILER32=${COMPILER32,,}
+			if [[ -z $COMPILER32 ]]; then
+				COMPILER32="clang"
+			fi
+			compiler_setup
+			;;
 		"--device="*)
 			CODE_NAME=${arg#*=}
 			case $CODE_NAME in
@@ -246,19 +251,15 @@ for arg in "$@"; do
 					DEVICENAME='Xiaomi 11 lite 5G NE'
 					CODENAME='lisa'
 					SUFFIX='qgki'
-					TARGET='Image'
-				;;
+					;;
 				*)
 					error 'device not supported'
 				;;
 			esac
 		;;
-		"--silence")
-			MAKE_ARGS+=("-s")
-		;;
-		"--pixel_thermals")
-			PIXEL_THERMALS='1'
-		;;
+		"--clean")
+			BUILD='clean'
+			;;
 		*)
 			usage
 		;;
